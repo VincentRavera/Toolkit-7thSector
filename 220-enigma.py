073c2270550c89dfe222d3e08152a288b1e9ee16
Vincent Ravera <ravera.vincent@gmail.com>
2019-12-16 01:01:11 +0100

    Cleaning up

--- 220-enigma.py
+++ 220-enigma.py
@@ -1,19 +1,20 @@
+#!/usr/bin/env python3
+# coding: utf-8
 import numpy as np
 
-
 x = np.array([0, 1])
-a = x*20
-b = x*35
-c = x*52
-d = x*81
-e = x*43
-f = x*90
+a = x * 20
+b = x * 35
+c = x * 52
+d = x * 81
+e = x * 43
+f = x * 90
 for i in a:
     for j in b:
         for k in c:
             for l in d:
                 for m in e:
                     for n in f:
-                            b = i+j+k+l+m+n
-                            if b == 220:
-                                print(i, j, k, l, m, n)
+                        b = i + j + k + l + m + n
+                        if b == 220:
+                            print(i, j, k, l, m, n)
--- operations.py
+++ operations.py
@@ -1,9 +1,12 @@
-op=['+', '-', '*', '/']
+#!/usr/bin/env python3
+# coding: utf-8
+
+op = ['+', '-', '*', '/']
 output = None
 for i in op:
     for j in op:
         for k in op:
-            stri = 'output=' + '((' + str(5.0) + i + str(8.0) + ')' + j + str(6.0) + ')' + k + str(2.0)
-            exec(stri)
+            executable_string = 'output=' + '((' + str(5.0) + i + str(8.0) + ')' + j + str(6.0) + ')' + k + str(2.0)
+            exec(executable_string)
             if output == 39:
-                print(stri, "=", output)
+                print(executable_string, "=", output)
